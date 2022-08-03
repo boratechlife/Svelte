9da341edc58b4957ccf8eb2db1f89cbf77364fe8
de <de@localhost.localdomain>
2022-08-03 06:38:09 -0400

    hook login to api

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,svelte,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-secondary': '#2a3042',
+        'gray-primary' : '#262B3C',
+        'dark' : '#222736',
+        'primary' : "#556EE6",
+        'success' : '#34C38F',
+        'orange' : '#F1B44C',
+        'danger' : '#F46A6A',
+        'dark-blue' : "#34406B"
+      },
+      fontFamily: {
+        'sans' : ['Poppins']
+      }
+    },
+  },
+  plugins: []
+};
